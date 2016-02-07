7d2cfbfd81851ab5086a5917d1b0ab762b29c79c
Arie <git@ariekanarie.nl>
2016-02-07 21:58:40 +0100

    be more specific which screen to stuff commands to

--- relaystatus.rb
+++ relaystatus.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-`screen -S #{ARGV[0]} -p 0 -X stuff 'tv_status'`
-`screen -S #{ARGV[0]} -p 0 -X hardcopy /tmp/#{ARGV[0]}.status`
+`screen -S #{ARGV[0]}.webrelay -p 0 -X stuff 'tv_status'`
+`screen -S #{ARGV[0]}.webrelay -p 0 -X hardcopy /tmp/#{ARGV[0]}.status`
